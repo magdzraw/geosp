6cc686b81d97d35741f98fa651276c44358cc509
magdzraw <maguirred2017@udec.cl>
2025-05-03 21:15:38 -0400

    R code and databases

---
+++ script_LACFORGEN_dataf_03-05.R
@@ -0,0 +1,127 @@
+library(sf)
+library(dplyr)
+library(readxl)
+library(tidyr)
+library(dplyr)
+library(openxlsx)
+library(leaflet)
+library(RColorBrewer)
+library(data.table)
+download.file(
+  "https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/world_shape_file.zip",
+  destfile = "shp/world_shape_file.zip"
+)
+system("unzip shp/world_shape_file.zip")
+world_sf <- read_sf(paste0(
+  getwd(), "/shp/",
+  "TM_WORLD_BORDERS_SIMPL-0.3.shp"
+))
+
+df <- read.xlsx("lacforgen.xlsx")
+
+
+df2 <- data.frame(
+  Investigador = df$Nombre.del.investigador.o.responsable.del.estudio,
+  Institucion = df$`Institución/organización`,
+  NAME = df$País.de.origen.del.estudio,
+  Especie = df$Especie.arborea.investigada,
+  Nombre_comun = df$Nombre.común.de.la.especie,
+  Estudio = df$Tipo.de.estudio.genético.realizado,
+  Marcadores = df$Marcadores.moleculares.utilizados,
+  stringsAsFactors = T
+  
+)
+
+df2 %>% 
+  group_by(Especie) %>% 
+  summarise(no_rows = length(Especie)) %>% print(n = 10)
+
+dftd <- df2 %>% 
+  separate(Especie, c("Genero", "epiteto"), sep = " ") %>% 
+  select(-'epiteto') %>% 
+  mutate(Especie = df2$Especie) %>% 
+  group_by(NAME)
+
+dftd %>% 
+  group_by(NAME) %>% 
+  summarise(Estudios_por_pais = length(Genero))
+
+epp <- dftd %>% 
+  select('NAME', 'Genero') %>% 
+  group_by(NAME) %>% 
+  summarise(Estudios_por_pais = length(Genero)) %>% 
+  arrange(desc(Estudios_por_pais))
+
+tip_est <- dftd %>% 
+  select('NAME', 'Estudio') %>% 
+  group_by(NAME)
+write.xlsx(tip_est, "tipos_estudio_pp.xlsx", colNames = TRUE)
+
+ntepp <- tip_est %>%
+  group_by(NAME, Estudio) %>%
+  summarise(No_estudios = n(), .groups = "drop") %>%
+  pivot_wider(names_from = Estudio, values_from = No_estudios, values_fill = 0)
+
+write.xlsx(ntepp, "conteo_por_pais.xlsx", colNames = TRUE)
+
+est_res <- ntepp %>% 
+  left_join(epp, by = 'NAME')
+setnames(est_res, "Estudios_por_pais", "Total de estudios")
+
+mapdata <- left_join(world_sf, est_res, by="NAME")
+
+mapdata1 <- mapdata %>% filter(!is.na(mapdata$`Total de estudios`))
+
+
+dftd <- dftd %>% 
+  select('region','Especie','Genero',
+         'Nombre_comun','Marcadores')
+
+mapdata <- left_join(world_sf, epp, by="NAME")
+
+mapdata1 <- mapdata %>% filter(!is.na(mapdata$`Estudios por pais`))
+
+colbins <- c(0, 3, 9, 12, 15, 18, 21, 24, 27, 30)
+paletacol <- colorBin(
+  palette = "YlOrRd", domain = mapdata1$`Total de estudios`,
+  na.color = "transparent", bins = colbins
+)
+paletacol(27)
+
+
+etiq <- paste(
+  "País: ", mapdata1$NAME, "<br/>",
+  "Total de estudios: ", mapdata1$`Total de estudios`, "<br/>",
+  "Diversidad genética: ", mapdata1$`Diversidad genética`, "<br/>",
+  "Estructura genética: ", mapdata1$`Estructura genética`, "<br/>",
+  "Flujo génico: ", mapdata1$`Flujo génico`, "<br/>",
+  "Filogenética: ", mapdata1$Filogenética, "<br/>",
+  "Filogeografía: ", mapdata1$Filogeografía, "<br/>",
+  "Otros: ", mapdata1$Otros, "<br/>",
+  
+  sep = ""
+) %>% 
+  lapply(htmltools::HTML)
+
+coromapfin <- leaflet(mapdata1) %>% 
+  addTiles() %>% 
+  setView(lat = -23.389, lng = -69.433, zoom = 3) %>% 
+  addPolygons(
+    fillColor = ~paletacol(`Total de estudios`),
+    stroke = FALSE, 
+    fillOpacity = 0.3,
+    color = "white",
+    weight = "0.2",
+    label = etiq,
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto"
+    )
+  ) %>% 
+  addLegend(
+    pal = paletacol,
+    values = ~`Total de estudios`, opacity = 0.3,
+    title = "Número de estudios", position = "bottomleft"
+  )
+
